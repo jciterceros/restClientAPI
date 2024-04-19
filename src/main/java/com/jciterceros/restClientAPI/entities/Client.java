d5bddf85f55c845bfbebd74465d05367f196209d
jciterceros <jciterceros@yahoo.com>
2024-04-19 13:07:02 -0300

    feat: created Entity Client with annotations

---
+++ src/main/java/com/jciterceros/restClientAPI/entities/Client.java
@@ -0,0 +1,106 @@
+package com.jciterceros.restClientAPI.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    @PastOrPresent
+    private LocalDate birthDate;
+    private Integer children;
+
+    public Client() {
+    }
+
+    public Client(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id) && Objects.equals(name, client.name) && Objects.equals(cpf, client.cpf) && Objects.equals(income, client.income) && Objects.equals(birthDate, client.birthDate) && Objects.equals(children, client.children);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, cpf, income, birthDate, children);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", income=" + income +
+                ", birthDate=" + birthDate +
+                ", children=" + children +
+                '}';
+    }
+}
